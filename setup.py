bb3153fd9dd8f43ced52346bdf23cc06bb4acf66
关明 <i@guanming.me>
2018-08-15 14:57:49 +0800

    dev

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name='pyexpr',
-    version='0.0.2',
+    version='0.0.2.1',
+    description='A python expression ecaluator',
+    url='https://github.com/lennon-guan/pyexpr',
     packages=['pyexpr'],
 )
