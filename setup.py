c3c68d2fb50586d0bc694216718b44e24155ec9e
关明 <i@guanming.me>
2019-03-11 19:17:43 +0800

    add version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyexpr',
-    version='0.0.2.1',
+    version='0.0.3.1',
     description='A python expression ecaluator',
     url='https://github.com/lennon-guan/pyexpr',
     packages=['pyexpr'],
